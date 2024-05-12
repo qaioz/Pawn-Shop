88f9c92bd5dc75eb24f260d8d32f0121ec8e0637
Gaioz <tabtadzegaga@gmail.com>
2024-05-12 21:26:53 +0400

    .

---
+++ src/main/java/dev/omedia/boot/BootApplication.java
@@ -0,0 +1,13 @@
+package dev.omedia.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+
+}
